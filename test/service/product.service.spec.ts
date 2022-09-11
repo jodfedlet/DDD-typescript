db70a620af8215173c824439a9f4381f4cc20c74
jodfedlet <fedletpierre15@gmail.com>
2022-09-11 14:10:36 -0300

    Init product service

---
+++ test/service/product.service.spec.ts
@@ -0,0 +1,15 @@
+import Product from "../../src/entity/product";
+import ProductService from "../../src/service/product.service";
+
+describe("Product service unit test", () => {
+  it("It should change the price of all products", () => {
+    const product1 = new Product("prod1", "Product 1", 10);
+    const product2 = new Product("prod2", "Product 2", 20);
+
+    const prods = [product1, product2];
+
+    ProductService.increasePrice(prods, 100);
+    expect(product1.price).toBe(20);
+    expect(product2.price).toBe(40);
+  });
+});
